6c4041a99a743ecff0519388411165b01856b206
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-24 21:03:09 +0500

    template literal

---
+++ index.js
@@ -0,0 +1,21 @@
+// let x= " test ".trim() //удаляет пробелы в начале и конце
+// console.log(x)
+
+// x = ' test'.trimStart() //удаляет пробелы в начале
+// x = 'test '.trimEnd() //удаляет пробелы в конце 
+
+// y = "test".padStart(3) //добавляет пробелы слева до длины 3
+// console.log(y)
+
+// y = 'test'.padEnd(3) //добавляет пробелы справа до длины 3
+
+// let x = [1, 2, 3, 4, 5]
+// let y = [6, 7, 8, 9]
+
+// console.log(x.concat(y)) // add "y" to "x"
+
+// console.log('l'.repeat(2)) //  repeat 'l' two times
+
+let name = "Humoyun"
+let x = `hello, my name is ${name}`
+console.log(x)
